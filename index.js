05d6fd6d74897927b28bf67237f315d4facc7cf2
Kris Wong <krischwong@gmail.com>
2022-07-08 23:51:00 -0400

    Added slider to change the size of the grid

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let canvasSize = 40;
+let root = document.documentElement;
+root.style.setProperty("--etch-size", canvasSize);
+
 let isClicked = false;
 const setClicked = () => {
   isClicked = !isClicked;
@@ -8,35 +12,62 @@ window.addEventListener("mouseup", setClicked);
 
 const container = document.querySelector(".container");
 
+const headerContainer = document.createElement("div");
+headerContainer.innerHTML = "Etch - A - Sketch";
+headerContainer.classList.add("header-container");
+container.appendChild(headerContainer);
+
 const etch = document.createElement("div");
 etch.classList.add("etch-container");
 container.appendChild(etch);
 
 const colourSquare = (e) => {
   if (isClicked) {
-    document.getElementById(e.target.id).style.backgroundColor = "green";
+    document.getElementById(e.target.id).style.backgroundColor = "#000000";
   }
 };
 
-for (let i = 0; i < 16 ** 2; i++) {
-  const box = document.createElement("div");
-  box.setAttribute("id", `box-${i}`);
-  box.classList.add("square");
-  box.addEventListener("mouseover", colourSquare);
-  box.addEventListener("click", (e) => {
-    document.getElementById(e.target.id).style.backgroundColor = "green";
-  });
-  etch.appendChild(box);
-}
+const createCanvas = (size) => {
+  etch.textContent = "";
+  root.style.setProperty("--etch-size", size);
+  for (let i = 0; i < size ** 2; i++) {
+    const box = document.createElement("div");
+    box.setAttribute("id", `box-${i}`);
+    box.classList.add("square");
+    box.addEventListener("mouseover", colourSquare);
+    box.addEventListener("mousedown", (e) => {
+      document.getElementById(e.target.id).style.backgroundColor = "#000000";
+    });
+    etch.appendChild(box);
+  }
+};
+
+createCanvas(canvasSize);
+
+const optionsDiv = document.createElement("div");
+optionsDiv.classList.add("options-container");
+container.appendChild(optionsDiv);
+
+const sliderDiv = document.createElement("div");
+sliderDiv.classList.add(".slider-container");
+optionsDiv.appendChild(sliderDiv);
+const sizeOption = document.createElement("input");
+sizeOption.setAttribute("type", "range");
+sizeOption.setAttribute("min", "16");
+sizeOption.setAttribute("max", "64");
+sizeOption.setAttribute("value", "40");
+sizeOption.addEventListener("input", (e) => {
+  createCanvas(e.target.value);
+  console.log(e.target.value);
+});
+sliderDiv.appendChild(sizeOption);
 
 const square = document.querySelectorAll(".square");
-
 const btn = document.createElement("button");
 btn.innerHTML = "reset";
 btn.addEventListener("click", (e) => {
   square.forEach((obj) => {
-    obj.style.backgroundColor = "aqua";
+    obj.style.backgroundColor = "#fff";
   });
 });
-
-container.appendChild(btn);
+optionsDiv.appendChild(btn);
